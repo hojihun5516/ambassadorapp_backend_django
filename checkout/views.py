58423dd719d4818e7ba989887a838417b46c2814
hojihun5516 <hojihun5516@daum.net>
2021-05-10 18:16:00 +0900

    Add: transaction order

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.shortcuts import render
 
 # Create your views here.
@@ -18,39 +19,49 @@ class LinkAPIView(APIView):
 
 
 class OrderAPIView(APIView):
+
+    @transaction.atomic
     def post(self, request):
         data = request.data
         link = Link.objects.filter(code=data['code']).first()
 
         if not link:
             raise exceptions.APIException('Invalid code!')
+        try:
+            order = Order()
+            order.code = link.code
+            order.user_id = link.user.id
+            order.ambassador_email = link.user.email
+            order.first_name = data['first_name']
+            order.last_name = data['last_name']
+            order.address = data['address']
+            order.email = data['email']
+            order.country = data['country']
+            order.city = data['city']
+            order.zip = data['zip']
+            order.save()
 
-        order = Order()
-        order.code = link.code
-        order.user_id = link.user.id
-        order.ambassador_email = link.user.email
-        order.first_name = data['first_name']
-        order.last_name = data['last_name']
-        order.address = data['address']
-        order.email = data['email']
-        order.country = data['country']
-        order.city = data['city']
-        order.zip = data['zip']
-        order.save()
+            for item in data['products']:
+                product = Product.objects.filter(pk=item['product_id']).first()
+                quantity = decimal.Decimal(item['quantity'])
 
-        for item in data['products']:
-            product = Product.objects.filter(pk=item['product_id']).first()
-            quantity = decimal.Decimal(item['quantity'])
-
-            order_item = OrderItem()
-            order_item.order = order
-            order_item.product_title = product.title
-            order_item.price = product.price
-            order_item.quantity = quantity
-            order_item.ambassador_revenue = decimal.Decimal(.1) * product.price * quantity
-            order_item.admin_revenue = decimal.Decimal(.9) * product.price * quantity
-            order_item.save()
+                order_item = OrderItem()
+                order_item.order = order
+                order_item.product_title = product.title
+                order_item.price = product.price
+                order_item.quantity = quantity
+                order_item.ambassador_revenue = decimal.Decimal(.1) * product.price * quantity
+                order_item.admin_revenue = decimal.Decimal(.9) * product.price * quantity
+                order_item.save()
+            return Response({
+                'message': 'success'
+            })
+        except Exception:
+            transaction.rollback()
 
         return Response({
-            'message':'success'
+            'message': 'Error occured'
         })
+
+
+
